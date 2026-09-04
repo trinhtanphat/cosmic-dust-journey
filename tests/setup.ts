31cf42b3a78a5ba3297310df57eef734260b02ea
trinhtanphat <89531707+trinhtanphat@users.noreply.github.com>
2026-09-04 13:30:44 +0700

    test: polyfill jsdom scroll and animation APIs

--- tests/setup.ts
+++ tests/setup.ts
@@ -13,3 +13,18 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: () => false,
   }),
 });
+
+Object.defineProperty(window, 'scrollTo', {
+  writable: true,
+  value: () => undefined,
+});
+
+Object.defineProperty(globalThis, 'requestAnimationFrame', {
+  writable: true,
+  value: (callback: FrameRequestCallback) => window.setTimeout(() => callback(performance.now()), 0),
+});
+
+Object.defineProperty(globalThis, 'cancelAnimationFrame', {
+  writable: true,
+  value: (id: number) => window.clearTimeout(id),
+});
